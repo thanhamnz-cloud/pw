fac9a355e3e2f8f232de3156c2f71d4a6fa50de2
Thanh Dinh Nguyen <thanh.dinhnguyen@hitachids.com>
2026-01-30 21:24:32 +0700

    fix

--- push.js
+++ push.js
@@ -12,7 +12,16 @@ push.setVapidDetails(
   vapidKeys.privateKey,
 );
 
-let sub = {};
+let sub = {
+  endpoint:
+    "https://fcm.googleapis.com/fcm/send/fKOhxIXx3Kw:APA91bEE6vW0hnZl_R_Qgfd514EOlBTbcIh-E_wa89i387ngpLq-yjj2RQ9oxAyS6WlM9THnK9ShOvra8BKS6RLLp_xOed4Q_I11UMcI_KE0rPMVWkYZPu-d02V4yboT-A7SWjYGYsbn",
+  expirationTime: null,
+  keys: {
+    p256dh:
+      "BM89x3etahEVMuBtVMIROvpEmWT8_4m7xn8vFBjHNNdu5qsw8xBVq8gQw1uD4CZDsbOvpQcu83Nd942v3tAz-UY",
+    auth: "pOSv73JFRjoZsLy1r82C3g",
+  },
+};
 push.sendNotification(sub, "Your Push Payload Text").catch((err) => {
   console.error(err.stack);
 });
--- sw.js
+++ sw.js
@@ -1,6 +1,27 @@
-self.addEventListener("push", () => {
-  console.log("Push event received");
-  self.registration.showNotification("[1] Push Notification", {
-    body: "[2] You have received a push notification.",
-  });
+self.addEventListener("push", function (e) {
+  var options = {
+    body: "This notification was generated from a push!",
+    icon: "images/example.png",
+    vibrate: [100, 50, 100],
+    data: {
+      dateOfArrival: Date.now(),
+      primaryKey: "2",
+    },
+    actions: [
+      {
+        action: "explore",
+        title: "Explore this new world",
+        icon: "images/checkmark.png",
+      },
+      {
+        action: "close",
+        title: "Close",
+        icon: "images/xmark.png",
+      },
+    ],
+  };
+
+  e.waitUntil(
+    self.registration.showNotification("[1] Push Notification", options),
+  );
 });
